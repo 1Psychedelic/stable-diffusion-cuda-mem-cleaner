b0ee33518a723f4e17645d1db73767b0ef968ab6
1Psychedelic <116289830+1Psychedelic@users.noreply.github.com>
2023-09-25 08:36:47 +0200

    Update clear_cuda_memory_ui.py

--- scripts/clear_cuda_memory_ui.py
+++ scripts/clear_cuda_memory_ui.py
@@ -1,30 +1,34 @@
 import modules.scripts as scripts
 import gradio as gr
-import torch
 import os
-import contextlib
+import torch
+
+from modules import images, script_callbacks
+from modules.processing import process_images, Processed
+from modules.processing import Processed
+from modules.shared import opts, cmd_opts, state
 
 class ExtensionTemplateScript(scripts.Script):
-    def title(self):
-        return "RESET CUDA MEMORY"
+        def title(self):
+                return "Extension Template"
 
-    def show(info, is_img2img):
-        return scripts.AlwaysVisible
+        def show(self, is_img2img):
+                return scripts.AlwaysVisible
+        
+        def reset_cuda_memory(self):
+                torch.cuda.empty_cache()
+                print('CUDA memory reset successfully!')
+                return "CUDA memory reset successfully!"
 
-    def reset_cuda_memory(self):  # Přidání 'self' jako prvního argumentu
-        torch.cuda.empty_cache()
-        return "CUDA memory reset successfully!"
+        # Setup menu ui detail
+        def ui(self, is_img2img):
+                with gr.Accordion('CUDA memory reset', open=False, elem_id=("cuda_reset")):
+                        with gr.Row():
+                                cuda_reset_button = gr.Button(value="RESET")
+                                cuda_reset_button.click(self.reset_cuda_memory)
+                return [cuda_reset_button]
 
-    def ui(self, is_img2img):
-        with gr.Accordion("RESET CUDA MEMORY", open=False):
-            with gr.Row():
-                reset_button = gr.Button("RESET")
-                info = gr.Info("Waiting for user action...")
+        def run(self, p, cuda_reset_button):
+                proc = process_images(p)
+                return proc
 
-        with contextlib.suppress(AttributeError):
-            if is_img2img:
-                reset_button.click(self.reset_cuda_memory, outputs=[info])  # Použití self.reset_cuda_memory
-            else:
-                reset_button.click(self.reset_cuda_memory, outputs=[info])
-
-        return [reset_button, info]
